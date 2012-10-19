a0004ff21ff823736905bfc829b597207c34b2b4
Thiago Augusto <ezequiel.c37@gmail.com>
2012-10-19 15:04:21 -0300

    Customização dos posts para página inicial

--- wp-content/themes/responsive/home.php
+++ wp-content/themes/responsive/home.php
@@ -24,6 +24,32 @@ if ( !defined('ABSPATH')) exit;
 <?php get_header(); ?>
 
         <div id="featured" class="grid col-940">
+
+        	<div id="rslider">
+	        	<ul class="rslides">
+					<li><? echo '<img class="aligncenter" src="'.get_stylesheet_directory_uri().'/slider/1_casamentos.jpg" />'; ?></li>
+					<li><? echo '<img class="aligncenter" src="'.get_stylesheet_directory_uri().'/slider/2_noivas.jpg" />'; ?></li>
+					<li><? echo '<img class="aligncenter" src="'.get_stylesheet_directory_uri().'/slider/3_romantico.jpg" />'; ?></li>
+					<li><? echo '<img class="aligncenter" src="'.get_stylesheet_directory_uri().'/slider/1_casamentos.jpg" />'; ?></li>
+				</ul>				
+			</div>
+
+			<?php $options = get_option('responsive_theme_options');
+			// First let's check if image was set
+			    if (!empty($options['featured_content'])) {
+					echo do_shortcode($options['featured_content']);
+		    // If not display dummy image for preview purposes
+			      } else {             
+                    echo '<img src="'.get_stylesheet_directory_uri().'/images/slider-bottom.png" width="960" height="68" alt="" />'; 
+ 				  }
+			?> 
+			
+
+        	<?php
+        		//if ( function_exists( 'get_smooth_slider' ) ) {
+	            //	get_smooth_slider();
+	            //}
+	        ?>
         
         <div class="grid col-460">
 
@@ -105,8 +131,73 @@ if ( !defined('ABSPATH')) exit;
 			?> 
                                    
         </div><!-- end of #featured-image --> 
-        
+
         </div><!-- end of #featured -->
+
                
 <?php get_sidebar('home'); ?>
+
+<!-- Blog ---------------------------- -->
+
+
+<div id="content" class="grid col-940">
+        
+	<?php if (have_posts()) : ?>
+
+		<?php while (have_posts()) : the_post(); ?>
+        
+            <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+            	<div class="post-meta">
+                	<?php responsive_post_meta_data(); ?>
+                </div><!-- end of .post-meta -->
+
+                <div class="post-data">
+				    <?php the_tags(__('Tagged with:', 'responsive') . ' ', ', ', '<br />'); ?> 
+					<?php printf(__('Posted in %s', 'responsive'), get_the_category_list(', ')); ?> 
+                </div><!-- end of .post-data -->
+
+                <?php if ( comments_open() ) : ?>
+                    <span class="comments-link">                    	
+                		<?php comments_popup_link(__('No Comments &darr;', 'responsive'), __('1 Comment &darr;', 'responsive'), __('% Comments &darr;', 'responsive')); ?>
+                    </span>
+                <?php endif; ?> 
+
+                <h1 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'responsive'), the_title_attribute('echo=0')); ?>"><?php the_title(); ?></a></h1>
+                
+                <div class="post-entry">
+                    <?php if ( has_post_thumbnail()) : ?>
+                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+                    <?php the_post_thumbnail(); ?>
+                        </a>
+                    <?php endif; ?>
+                    <?php the_content(__('Read more &#8250;', 'responsive')); ?>
+                    <?php wp_link_pages(array('before' => '<div class="pagination">' . __('Pages:', 'responsive'), 'after' => '</div>')); ?>
+                </div><!-- end of .post-entry -->               
+                
+            	<div class="post-edit"><?php edit_post_link(__('Edit', 'responsive')); ?></div>               
+            </div><!-- end of #post-<?php the_ID(); ?> -->
+            
+        <?php endwhile; ?> 
+        
+        <?php if (  $wp_query->max_num_pages > 1 ) : ?>
+        <div class="navigation">
+			<div class="previous"><?php next_posts_link( __( '&#8249; Older posts', 'responsive' ) ); ?></div>
+            <div class="next"><?php previous_posts_link( __( 'Newer posts &#8250;', 'responsive' ) ); ?></div>
+		</div><!-- end of .navigation -->
+        <?php endif; ?>
+
+	    <?php else : ?>
+
+        <h1 class="title-404"><?php _e('404 &#8212; Fancy meeting you here!', 'responsive'); ?></h1>
+        <p><?php _e('Don\'t panic, we\'ll get through this together. Let\'s explore our options here.', 'responsive'); ?></p>
+        <h6><?php _e( 'You can return', 'responsive' ); ?> <a href="<?php echo home_url(); ?>/" title="<?php esc_attr_e( 'home', 'responsive' ); ?>"><?php _e( '&larr; Home', 'responsive' ); ?></a> <?php _e( 'or search for the page you were looking for', 'responsive' ); ?></h6>
+        <?php get_search_form(); ?>
+        
+	<?php endif; ?>  
+      
+</div><!-- end of #content -->
+
+
+
 <?php get_footer(); ?>
